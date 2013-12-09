b870eeb24fc75f47f667f21e9c9f4b6df9a14fe2
Antonia Mey <antonia.mey@fu-berlin.de>
2013-12-09 10:58:20 +0100

    some more cosmetic changes

--- src/Observables/Observable.py
+++ src/Observables/Observable.py
@@ -1,7 +1,32 @@
-'''
-Created on 09.12.2013
+#!/usr/bin/env python
 
-@author: ppxasjsm
+'''
+    File:            /home/mi/ppxasjsm/Eclipse/MD_Observables/src/Observables/Observable.py
+    Description:
+    
+    Author:          Antonia Mey    
+    Email:           antonia.mey@fu-berlin.de      
+    Date:            09.12.2013
+    Version:         0.0.1 Beta
+    
+    #=============================================================================================
+    # COPYRIGHT NOTICE
+    #
+    # Written by Antonia Mey <antonia.mey@fu-berlin.de>
+    #
+    # This program is free software; you can redistribute it and/or modify it under the terms of
+    # the GNU General Public License as published by the Free Software Foundation; either version 2
+    # of the License, or (at your option) any later version.
+    #
+    # This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+    # without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    # See the GNU General Public License for more details.
+    # 
+    # You should have received a copy of the GNU General Public License along with this program;
+    # if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    # Boston, MA  02110-1301, USA.
+    #=============================================================================================
+
 '''
 from MDAnalysis import *
 import os
